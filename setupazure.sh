91205d78b8a733aceb7667f5bf2da1e5b2eba45c
Dask <82965907+DaskSs@users.noreply.github.com>
2023-12-05 23:07:42 -0300

    Update setupazure.sh

--- setupazure.sh
+++ setupazure.sh
@@ -24,40 +24,10 @@ python3.10 -m pip install discord requests Pillow httpx asyncio
 # Instala o pacote 'sydney-py'
 python3.10 -m pip install sydney-py
 
-# Instala o vsftpd
-sudo apt-get install -y vsftpd
-
-# Configurações básicas do vsftpd (adicione ou ajuste conforme necessário)
-sudo bash -c 'echo "listen=NO" >> /etc/vsftpd.conf'
-sudo bash -c 'echo "listen_ipv6=YES" >> /etc/vsftpd.conf'
-sudo bash -c 'echo "anonymous_enable=NO" >> /etc/vsftpd.conf'
-sudo bash -c 'echo "local_enable=YES" >> /etc/vsftpd.conf'
-sudo bash -c 'echo "write_enable=YES" >> /etc/vsftpd.conf'
-sudo bash -c 'echo "chroot_local_user=YES" >> /etc/vsftpd.conf'
-sudo bash -c 'echo "local_umask=022" >> /etc/vsftpd.conf'
-sudo bash -c 'echo "user_sub_token=\$USER" >> /etc/vsftpd.conf'
-sudo bash -c 'echo "local_root=/home/\$USER/ftp" >> /etc/vsftpd.conf'
-
-# Reinicia o serviço vsftpd
-sudo systemctl restart vsftpd
-
-# Adiciona o usuário ftpuser com a senha 12345
-sudo adduser ftpuser --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-echo "ftpuser:12345" | sudo chpasswd
-
 # Cria a pasta AutoADS e subpastas
 sudo mkdir -p /AutoADS/python
 sudo chown ftpuser:ftpuser /AutoADS
 sudo chown ftpuser:ftpuser /AutoADS/python
-
-# Configurações adicionais para o diretório do usuário FTP
-sudo mkdir /home/ftpuser/ftp
-sudo chown ftpuser:ftpuser /home/ftpuser/ftp
-sudo chmod a-w /home/ftpuser/ftp
-
-# Cria um diretório para upload (opcional)
-sudo mkdir /home/ftpuser/ftp/files
-sudo chown ftpuser:ftpuser /home/ftpuser/ftp/files
 
 # Instala o Git
 sudo apt-get install -y git
